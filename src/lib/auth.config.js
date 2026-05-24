ac7b7b176c945b567ab6eaccd073ed487478287b
Gaber Usef <dev.gaber@gmail.com>
2026-05-24 23:58:41 +0300

    fix redirect to localhost

--- src/lib/auth.config.js
+++ src/lib/auth.config.js
@@ -1,24 +1,23 @@
 export const authConfig = {
-  providers: [], // Credentials providers are added in auth.js to avoid loading edge-incompatible modules here
+  trustHost: true,
+  providers: [],
   callbacks: {
-    // 🌟 خطوة الإنقاذ الأولى في الـ JWT
-    async jwt({token, user}) {
+    async jwt({ token, user }) {
       if (user) {
         token.id = user.id;
-        token.role = user.role; // ننقلها يدويًا للتوكن
-        token.status = user.status; // ننقلها يدويًا للتوكن
+        token.role = user.role;
+        token.status = user.status;
         token.phone_number = user.phone_number;
         token.country = user.country;
       }
       return token;
     },
 
-    // 🌟 خطوة الإنقاذ الثانية في الـ Session
     async session({session, token}) {
       if (token && session.user) {
         session.user.id = token.id;
-        session.user.role = token.role; // ننقلها يدويًا للجلسة العلنية
-        session.user.status = token.status; // ننقلها يدويًا للجلسة العلنية
+        session.user.role = token.role;
+        session.user.status = token.status;
         session.user.phone_number = token.phone_number;
         session.user.country = token.country;
       }
